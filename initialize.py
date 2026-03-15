97734cc859d18f68a81a15862ce187a82a608cf4
takumi7821 <takumi7821@gmail.com>
2026-03-15 12:52:00 +0900

    first commit.

--- initialize.py
+++ initialize.py
@@ -15,7 +15,7 @@ import tiktoken
 from langchain_openai import ChatOpenAI
 from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 from langchain_community.utilities import SerpAPIWrapper
-from langchain.tools import Tool
+from langchain_core.tools import Tool
 from langchain.agents import AgentType, initialize_agent
 import utils
 import constants as ct
